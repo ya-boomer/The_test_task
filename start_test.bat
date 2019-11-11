b7720a8dee979e385fc3463ef09aee19d676cb98
Mishchenko Aleksey <53175668+ya-boomer@users.noreply.github.com>
2019-11-11 23:17:59 +0300

    Add start_test.bat
    Bat-file for run autotest

---
+++ start_test.bat
@@ -0,0 +1,11 @@
+@echo off
+@Rem Check for the folder path specified as parameter%1
+@Rem If parameter%1 is empty, go to the error label
+if "%1" EQU "" goto error 
+newman run %1\autotest.json -d %1\data.json
+exit
+:error 
+@Rem Specify current folder
+echo variable not set
+newman run %~dp0\autotest.json -d %~dp0\data.json
+:exit
